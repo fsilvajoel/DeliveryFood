1688e4fa69c301b365f310ca609c7168f9187558
js69031 <joelsilva_compasso@getnet.com.br>
2020-07-06 01:38:14 -0300

    Adicionado recurso de Tema personalizado
    modificação do json do Mock

--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes'
 import * as serviceWorker from './serviceWorker';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+// import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary:{ main : '#000000'},
+    secondary:{ main : '#ea1d2c'} ,
+  },
+  status: {
+    danger: 'orange',
+  },
+});
 
 ReactDOM.render(
+  
   <React.StrictMode>
-     <Routes/>
+    <ThemeProvider theme={theme}>
+      <Routes/>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
