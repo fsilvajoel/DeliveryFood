00c84ce2d979bcc1478b4c8684e952bcf475b6f5
fsilvajoel <jfsilva@inf.ufsm.br>
2020-09-20 22:34:18 -0300

    organização do redux

--- src/Redux/Store/Products/ProductsDucks.js
+++ src/Redux/Store/Products/ProductsDucks.js
@@ -1,38 +1,38 @@
 // types
 
-export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
-export const SET_FOOD = 'SET_FOOD';
-export const SET_CURRENT_CATEGORY = 'SET_CURRENT_CATEGORY';
+export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
+export const SET_FOOD = 'SET_FOOD'
+export const SET_CURRENT_CATEGORY = 'SET_CURRENT_CATEGORY'
 
 const initialState = {
   food: {},
   drink: {},
-  current_category: {}
-};
+  current_category: {},
+}
 // reducer
 export default function products(state = initialState, action) {
   switch (action.type) {
     case GET_ALL_PRODUCTS:
       return {
         ...state,
-        food: action.payload.Comida,
-        drink: action.payload.Bebida
-      };
+        food: action.payload[0],
+        drink: action.payload[1],
+      }
     case SET_CURRENT_CATEGORY:
       return {
         ...state,
-        current_category: action.payload
-      };
+        current_category: action.payload,
+      }
     default:
-      return state;
+      return state
   }
 }
 // actions
 export const getAllProducts = (products) => ({
   type: GET_ALL_PRODUCTS,
-  payload: products
-});
+  payload: products,
+})
 export const setCurrentCategory = (category) => ({
   type: SET_CURRENT_CATEGORY,
-  payload: category
-});
+  payload: category,
+})
