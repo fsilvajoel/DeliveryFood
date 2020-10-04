224618a2c565356ac6837087d3c51975007d9d10
fsilvajoel <jfsilva@inf.ufsm.br>
2020-10-04 02:40:14 -0300

    evolução do checkout

--- src/Redux/Store/CheckoutCart/CheckoutCart.js
+++ src/Redux/Store/CheckoutCart/CheckoutCart.js
@@ -1,8 +1,12 @@
 const SET_PRODUCT = 'SET_PRODUCT'
+const SET_PAYMENT = 'SET_PAYMENT'
+const SET_DELIVERY = 'SET_DELIVERY'
 const DELETE_SHOPPING_CART = 'DELETE_SHOPPING_CART'
 
 const initialState = {
   shoppingCart: [],
+  paymentMethod: [],
+  deliveryMethod: [],
 }
 
 export default function checkoutCart(state = initialState, action) {
@@ -11,6 +15,16 @@ export default function checkoutCart(state = initialState, action) {
       return {
         ...state,
         shoppingCart: [...state.shoppingCart, action.payload],
+      }
+    case SET_PAYMENT:
+      return {
+        ...state,
+        paymentMethod: action.payload,
+      }
+    case SET_DELIVERY:
+      return {
+        ...state,
+        deliveryMethod: action.payload,
       }
     case DELETE_SHOPPING_CART:
       // state.shoppingCart.splice(action.payload, 1)
@@ -25,6 +39,14 @@ export default function checkoutCart(state = initialState, action) {
   }
 }
 // actions
+export const setDelivery = (product) => ({
+  type: SET_DELIVERY,
+  payload: product,
+})
+export const setPayment = (product) => ({
+  type: SET_PAYMENT,
+  payload: product,
+})
 export const setNewProduct = (product) => ({
   type: SET_PRODUCT,
   payload: product,
--- src/pages/OrderCheckout/resume.jsx
+++ src/pages/OrderCheckout/resume.jsx
@@ -5,10 +5,13 @@ import CardPaymentMethod from '../../Components/CardsCheckout/CardPaymentMethod'
 export default function PaymentForm() {
   return (
     <>
-      <Typography variant="h6" gutterBottom style={{ textAlign: "center" }} >
+      <Typography variant="h6" gutterBottom style={{ textAlign: "center", padding: "20px" }} >
         Forma de Pagamento
       </Typography>
       <CardPaymentMethod />
+      <Typography variant="h6" gutterBottom style={{ textAlign: "center", padding: "20px" }} >
+        Forma de Entrega
+      </Typography>
       <CardDeliveryMethod />
     </>
   );
