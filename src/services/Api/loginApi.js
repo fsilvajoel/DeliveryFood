72dd4243a60ca3c5944d6af46c66d60c01a84c3c
fsilvajoel <jfsilva@inf.ufsm.br>
2020-10-13 00:16:14 -0300

    criado POST de criação de usuário

--- src/services/Api/loginApi.js
+++ src/services/Api/loginApi.js
@@ -1,10 +1,19 @@
 import Axios from 'axios'
 import { apiUrl } from './apiConstants'
 
-const deliveryPath = `${apiUrl}/rest-auth/login/`
+const deliveryPathAuth = `${apiUrl}/rest-auth/login/`
+const deliveryPathRegistration = `${apiUrl}/rest-auth/registration/`
 
 export async function login(Userdata) {
-  const response = await Axios.post(`${deliveryPath}`, Userdata).catch((err) => Promise.reject(new Error(err)))
+  const response = await Axios.post(`${deliveryPathAuth}`, Userdata).catch((err) => Promise.reject(new Error(err)))
+  console.log('response', response)
+  return response
+}
+
+export async function registration(Userdata) {
+  const response = await Axios.post(`${deliveryPathRegistration}`, Userdata).catch((err) =>
+    Promise.reject(new Error(err))
+  )
   console.log('response', response)
   return response
 }
