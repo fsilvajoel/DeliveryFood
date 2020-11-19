05996fb5e25761da939bdbafab4a2391175d974e
fsilvajoel <jfsilva@inf.ufsm.br>
2020-11-19 00:29:43 -0300

    edição dos campos de cadastro

--- src/pages/Login/SignUp.jsx
+++ src/pages/Login/SignUp.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react'
-import Box from '@material-ui/core/Box'
-import Button from '@material-ui/core/Button'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import TextField from '@material-ui/core/TextField'
-import Link from '@material-ui/core/Link'
-import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid'
-import { makeStyles } from '@material-ui/core/styles'
-import { useForm } from 'react-hook-form'
+import React, { useState } from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import { useForm } from 'react-hook-form';
 // import FacebookIcon from '@material-ui/icons/Facebook'
-import Copyright from '../../Components/Layout/Copyright'
-import backgroundLogin from './images/backgroundLogin.jpg'
-import logoMorita from './images/morita_logo.png'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import { registration } from '../../services/Api/loginApi'
+import Copyright from '../../Components/Layout/Copyright';
+import backgroundLogin from './images/backgroundLogin.jpg';
+import logoMorita from './images/morita_logo.png';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { registration } from '../../services/Api/loginApi';
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
@@ -60,82 +60,93 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-}))
+}));
 
 export default function SignUp() {
-  const classes = useStyles()
-  const { register, handleSubmit, watch, errors } = useForm()
+  const classes = useStyles();
+  const { register, handleSubmit, watch, errors } = useForm();
   const onSubmit = (data) => {
     // setLoading(true);
-    console.log(data)
-    registration(data)
-  }
-  const [loading, setLoading] = useState(false)
+    console.log(data);
+    registration(data);
+  };
+  const [loading, setLoading] = useState(false);
   return (
-    <Grid container component='main' className={classes.root}>
+    <Grid container component="main" className={classes.root}>
       <CssBaseline />
       {loading && (
         <div className={classes.loading}>
-          <CircularProgress color='secondary' />
+          <CircularProgress color="secondary" />
         </div>
       )}
       <Grid component={Paper} elevation={6} item md={5} sm={8} square xs={12}>
         <div className={classes.paper}>
-          <img className={classes.logo} src={logoMorita} alt='Logo' />
+          <img className={classes.logo} src={logoMorita} alt="Logo" />
           <h1 className={classes.title}>Cadastrar-se</h1>
           <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
             <TextField
-              autoComplete='Nome'
+              autoComplete="Nome"
               autoFocus
               fullWidth
-              label='Crie um username. Ex: @morita'
-              margin='normal'
-              name='username'
+              label="Seu primeiro nome"
+              margin="normal"
+              name="firstname"
               required
-              variant='outlined'
+              variant="outlined"
               inputRef={register}
             />
             <TextField
-              autoComplete='email'
+              autoComplete="Sobrenome"
               autoFocus
               fullWidth
-              label='Endereço de E-mail'
-              margin='normal'
-              name='email'
+              label="Sobrenome"
+              margin="normal"
+              name="lastname"
               required
-              variant='outlined'
+              variant="outlined"
               inputRef={register}
             />
             <TextField
-              autoComplete='current-password'
+              autoComplete="email"
+              autoFocus
               fullWidth
-              label='Senha'
-              margin='normal'
-              name='password1'
+              label="Endereço de E-mail"
+              margin="normal"
+              name="email"
               required
-              type='password'
-              variant='outlined'
+              variant="outlined"
               inputRef={register}
             />
             <TextField
-              autoComplete='current-password'
+              autoComplete="current-password"
               fullWidth
-              label='Confirmar Senha'
-              margin='normal'
-              name='password2'
+              label="Senha"
+              margin="normal"
+              name="password1"
               required
-              type='password'
-              variant='outlined'
+              type="password"
+              variant="outlined"
+              inputRef={register}
+            />
+            <TextField
+              autoComplete="current-password"
+              fullWidth
+              label="Confirmar Senha"
+              margin="normal"
+              name="password2"
+              required
+              type="password"
+              variant="outlined"
               inputRef={register}
             />
             {errors.password && <span>Informe sua senha</span>}
-            <Button className={classes.submit} color='primary' fullWidth type='submit' variant='contained'>
+            <Button className={classes.submit} color="primary" fullWidth type="submit" variant="contained">
               Pronto
             </Button>
             <Grid container>
               <Grid item xs></Grid>
               <Grid item>
-                <Link href='/login' variant='body2'>
+                <Link href="/login" variant="body2">
                   Já cadastrado? Entrar!
                 </Link>
               </Grid>
@@ -148,5 +159,5 @@ export default function SignUp() {
       </Grid>
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
     </Grid>
-  )
+  );
 }
