de434288ab2b9a4160b48936a8cc9577074dd564
fsilvajoel <jfsilva@inf.ufsm.br>
2020-12-07 19:41:55 -0300

    criação da listagem de complementos

--- src/Components/OrderCheckout/cardComplements.jsx
+++ src/Components/OrderCheckout/cardComplements.jsx
@@ -1,27 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
+import styled from 'styled-components';
+const List = styled.li`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  list-style-type: none;
+`;
+const Button = styled.button`
+  background-color: white;
 
+  border: 2px solid #757575;
+  border-radius: 3px;
+  width: 25px;
+  height: 25px;
+  margin: 3px;
+`;
+const Count = styled.div`
+  input {
+    border: 2px solid #757575;
+    border-radius: 3px;
+    width: 25px;
+    height: 25px;
+    text-align: center;
+  }
+`;
 function CardComplements() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <h3>Selecione seus Complementos desejados</h3>
-      <li>
-        <Checkbox />
-        Hashi
-      </li>
-      <li>
-        <Checkbox />
-        Hashi
-      </li>
-      <li>
-        <Checkbox />
-        Hashi
-      </li>
-      <li>
-        <Checkbox />
-        Hashi
-      </li>
-      <p>chekcbox, nome botão + -</p></>
+      <h3>Selecione os Complementos desejados</h3>
+      <List>
+        <div>
+          <Checkbox />
+          Hashi
+        </div>
+        <Count>
+          <input value={count} />
+          <Button onClick={() => setCount(count + 1)}> +</Button>
+          <Button onClick={() => setCount(count > 0 ? count - 1 : 0)}> -</Button>
+        </Count>
+      </List>
+      <List>
+        <div>
+          <Checkbox />
+          Shoyu
+        </div>
+        <Count>
+          <input value={count} />
+          <Button onClick={() => setCount(count + 1)}> +</Button>
+          <Button onClick={() => setCount(count > 0 ? count - 1 : 0)}> -</Button>
+        </Count>
+      </List>
+      <List>
+        <div>
+          <Checkbox />
+          Wakame
+        </div>
+        <Count>
+          <input value={count} />
+          <Button onClick={() => setCount(count + 1)}> +</Button>
+          <Button onClick={() => setCount(count > 0 ? count - 1 : 0)}> -</Button>
+        </Count>
+      </List>
+      <List>
+        <div>
+          <Checkbox />
+          Wasabi
+        </div>
+        <Count>
+          <input value={count} />
+          <Button onClick={() => setCount(count + 1)}> +</Button>
+          <Button onClick={() => setCount(count > 0 ? count - 1 : 0)}> -</Button>
+        </Count>
+      </List>
+    </>
   );
 }
 
