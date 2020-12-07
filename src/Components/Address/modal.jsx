df20ddaeda830596e03dcde24445a61c2563bebf
fsilvajoel <jfsilva@inf.ufsm.br>
2020-12-07 19:38:49 -0300

    modal de endereços

--- src/Components/Address/modal.jsx
+++ src/Components/Address/modal.jsx
@@ -1,38 +1,30 @@
-import React, { useState } from 'react'
-
-import Button from '@material-ui/core/Button'
-import CloseIcon from '@material-ui/icons/Close'
-import Modal from '@material-ui/core/Modal'
-// import ModalBody from './ModalBody'
-import { makeStyles } from '@material-ui/core/styles'
-import AddIcon from '@material-ui/icons/Add'
-
-// import Box from '@material-ui/core/Box'
-// import CssBaseline from '@material-ui/core/CssBaseline'
-import TextField from '@material-ui/core/TextField'
-// import Link from '@material-ui/core/Link'
-// import Paper from '@material-ui/core/Paper'
-// import Grid from '@material-ui/core/Grid'
-// import { makeStyles } from '@material-ui/core/styles'
-import { useForm } from 'react-hook-form'
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import CloseIcon from '@material-ui/icons/Close';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import TextField from '@material-ui/core/TextField';
+import { getStorageItem } from '../../services/storage/index';
+import { useForm } from 'react-hook-form';
 
 export default function ModalAdress(props) {
-  const { register, handleSubmit, watch, errors } = useForm()
+  const { register, handleSubmit, errors } = useForm();
+  const cityes = useSelector((data) => data.adressStore.cities);
+  const districts = useSelector((data) => data.adressStore.districts);
+  console.log('CIDADES', cityes);
+  console.log('bairros', districts);
+
   const onSubmit = (data) => {
-    // setLoading(true);
-    console.log(data)
-  }
-  // "token": "86eb2b78ae047692522f5496cf90a537e6112442",
-  // "name": "Minha casa",
-  // "street": "Tuiuti",
-  // "number": 2230,
-  // "complement": "ap201",
-  // "reference": "perto do restaurante",
+    const token = getStorageItem('token-info');
+    var result = Object.assign(token, data);
+    console.log(data);
+    console.log('a resposta é', result);
+  };
   // "city": 1,
   // "district": 1,
-  // "cep": 97050420
 
-  // getModalStyle is not a pure function, we roll the style only on the first render
   const getModalStyle = () => {
     return {
       top: `50%`,
@@ -42,8 +34,8 @@ export default function ModalAdress(props) {
       flexDirection: 'row-reverse',
       textAlign: 'center',
       border: 'none',
-    }
-  }
+    };
+  };
 
   const useStyles = makeStyles((theme) => ({
     paper: {
@@ -86,91 +78,90 @@ export default function ModalAdress(props) {
       fontWeight: '100',
       textAlign: 'justify',
     },
-  }))
+  }));
 
-  const classes = useStyles()
-  const [active, setActive] = useState(false)
-  const [modalStyle] = useState(getModalStyle)
+  const classes = useStyles();
+  const [active, setActive] = useState(false);
+  const [modalStyle] = useState(getModalStyle);
 
   const actived = () => {
-    setActive(!active)
-  }
+    setActive(!active);
+  };
 
   return (
     <div>
-      <Button size='small' onClick={actived} color='primary'>
+      <Button size="small" onClick={actived} color="primary">
         <AddIcon />
       </Button>
       <Modal open={active} onClose={actived}>
-        <div key='modal' style={modalStyle} className={classes.paper}>
+        <div key="modal" style={modalStyle} className={classes.paper}>
           <button className={classes.button} onClick={actived}>
             <CloseIcon />
           </button>
-          <div id='simple-modal-description' className={classes.simpleModal}>
+          <div id="simple-modal-description" className={classes.simpleModal}>
             <div className={classes.content}>
               {/* <div className={classes.paper}> */}
               <h1 className={classes.title}>Cadastrar novo Endereço</h1>
               <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
                 <TextField
-                  autoComplete='Nome'
+                  autoComplete="Nome"
                   autoFocus
                   fullWidth
-                  label='Nome do novo Endereço'
-                  margin='normal'
-                  name='name'
+                  label="Nome do novo Endereço"
+                  margin="normal"
+                  name="name"
                   required
-                  variant='outlined'
+                  variant="outlined"
                   inputRef={register}
                 />
                 <TextField
-                  autoComplete='Rua'
-                  autoFocus
+                  autoComplete="Rua"
                   fullWidth
-                  label='Rua ou logradouro'
-                  margin='normal'
-                  name='street'
+                  label="Rua ou logradouro"
+                  margin="normal"
+                  name="street"
                   required
-                  variant='outlined'
+                  variant="outlined"
                   inputRef={register}
                 />
                 <TextField
-                  autoComplete='Número'
+                  autoComplete="Número"
                   fullWidth
-                  label='Número'
-                  margin='normal'
-                  name='number'
+                  label="Número"
+                  margin="normal"
+                  name="number"
                   required
-                  variant='outlined'
+                  variant="outlined"
                   inputRef={register}
                 />
                 <TextField
-                  autoComplete='Complemento'
+                  autoComplete="Complemento"
                   fullWidth
-                  label='Complemento'
-                  margin='normal'
-                  name='complement'
+                  label="Complemento"
+                  margin="normal"
+                  name="complement"
                   required
-                  variant='outlined'
+                  variant="outlined"
                   inputRef={register}
                 />
                 <TextField
-                  autoComplete='Ex: em frente ao mercado'
+                  autoComplete="Ex: em frente ao mercado"
                   fullWidth
-                  label='Referência'
-                  margin='normal'
-                  name='reference'
+                  label="Referência"
+                  margin="normal"
+                  name="reference"
                   required
-                  variant='outlined'
+                  variant="outlined"
                   inputRef={register}
                 />
                 <TextField
-                  autoComplete='CEP'
+                  autoComplete="CEP"
                   fullWidth
-                  label='cep'
-                  margin='normal'
-                  name='cep'
+                  label="cep"
+                  margin="normal"
+                  name="cep"
                   required
-                  variant='outlined'
+                  variant="outlined"
                   inputRef={register}
                 />
                 <select>
@@ -184,7 +175,7 @@ export default function ModalAdress(props) {
                   <option>Bairro</option>
                 </select>
                 {errors.password && <span>Informe sua senha</span>}
-                <Button className={classes.submit} color='primary' fullWidth type='submit' variant='contained'>
+                <Button className={classes.submit} color="primary" fullWidth type="submit" variant="contained">
                   Pronto
                 </Button>
               </form>
@@ -194,5 +185,5 @@ export default function ModalAdress(props) {
         </div>
       </Modal>
     </div>
-  )
+  );
 }
