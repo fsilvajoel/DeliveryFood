5d5a9fbd6fefc76a06ce4d087685edee41ba5bf4
fsilvajoel <jfsilva@inf.ufsm.br>
2020-12-07 19:39:36 -0300

    evolução do modal de sabores

--- src/Components/Modal/FlavorsList.jsx
+++ src/Components/Modal/FlavorsList.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 import { CardActions, CardContent } from '@material-ui/core';
-import { useDispatch, useSelector } from 'react-redux'
-import Button from "@material-ui/core/Button";
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '@material-ui/core/Button';
 import { List, ListItemText, ListItem } from '@material-ui/core';
-import "./style.scss"
-import { makeStyles } from "@material-ui/core/styles";
-import { setNewProduct } from '../../Redux/Store/CheckoutCart/CheckoutCart'
+import './style.scss';
+import { makeStyles } from '@material-ui/core/styles';
+import { setNewProduct } from '../../Redux/Store/CheckoutCart/CheckoutCart';
 import { useForm } from 'react-hook-form';
 
 const useStyles = makeStyles((theme) => ({
   listItem: {
     padding: theme.spacing(1, 0),
-    borderBottom: "1px solid #e8e8e8",
+    borderBottom: '1px solid #e8e8e8',
   },
   total: {
     fontWeight: 700,
@@ -20,36 +20,35 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(2),
   },
   iconDelete: {
-    fontSize: "10px",
+    fontSize: '10px',
   },
   error: {
-    textAlign: "center",
-    color: "red"
-  }
+    textAlign: 'center',
+    color: 'red',
+  },
 }));
 
 export default function FlavorsList(props) {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const { product } = props
+  const { product } = props;
 
   const classes = useStyles();
 
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => {
-    let flavor = JSON.parse(data.flavor)
+  const onSubmit = (data) => {
+    let flavor = JSON.parse(data.flavor);
     let SelectedProduct = {
-      'name': product.name,
-      'description': product.description,
-      'image': product.image,
-      'price': product.price,
-      'flavor': flavor,
-      'totalPrice': (Number(product.price) + Number(flavor.value)).toFixed(2)
+      name: product.name,
+      description: product.description,
+      image: product.image,
+      price: product.price,
+      flavor: flavor,
+      totalPrice: (Number(product.price) + Number(flavor.value)).toFixed(2),
     };
-    dispatch(setNewProduct(SelectedProduct))
-    props.modal()
-
-  }
+    dispatch(setNewProduct(SelectedProduct));
+    props.modal();
+  };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <CardContent>
@@ -57,11 +56,15 @@ export default function FlavorsList(props) {
           {product?.flavors.map((flavor, key) => (
             <>
               <ListItem className={classes.listItem} key={key}>
-                <ListItemText className="flavors-list" primary={flavor.name} secondary={`+R$: ${flavor.sizes[0].value}`} />
+                <ListItemText
+                  className="flavors-list"
+                  primary={flavor.name}
+                  secondary={`+R$: ${flavor.sizes[0].value}`}
+                />
                 <input
                   type="radio"
                   placeholder="flavors"
-                  name='flavor'
+                  name="flavor"
                   value={`{"sabor":"${flavor.name}","value": ${flavor.sizes[0].value}}`}
                   ref={register({ required: true })}
                 />
@@ -70,8 +73,10 @@ export default function FlavorsList(props) {
           ))}
           {errors.flavor && <p className={classes.error}>Selecione o sabor desejado</p>}
         </List>
-        <CardActions style={{ flexDirection: "row-reverse" }}>
-          <Button type="submit" variant="outlined" size="medium">Finalizar</Button>
+        <CardActions style={{ flexDirection: 'row-reverse' }}>
+          <Button type="submit" variant="outlined" size="medium">
+            Finalizar
+          </Button>
         </CardActions>
       </CardContent>
     </form>
